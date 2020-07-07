37cbfa2c7de9badcbb85681826bdae7ee0497d68
mrinal bedi <mbedi2291@conestogac.on.ca>
2020-07-07 15:30:31 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tikti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
