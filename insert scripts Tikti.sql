49f7b0fd0df2b431cc155876699ef5b6c62d0af1
mrinalbedi <67865511+mrinalbedi@users.noreply.github.com>
2020-08-05 23:02:08 -0400

    Alternate Titles inserted for first 4 SOC Codes

---
+++ insert scripts Tikti.sql
@@ -0,0 +1,232 @@
+--Alternate Titles Inserts
+
+delete from alternateTitles
+DBCC CHECKIDENT ('alternateTitles', RESEED, 0)
+
+--////11-3021.00------------------------------
+insert into alternateTitles values ('Applciation Development Director','11-3021.00')
+insert into alternateTitles values ('Chief Information Officer','11-3021.00')
+insert into alternateTitles values ('Chief Technology Officer (CTO)','11-3021.00'),
+('Computer Operations Manager','11-3021.00'),
+('Computer Programming Manager','11-3021.00'),
+('Computer Security Manager','11-3021.00'),
+('Computer Systems Information Director','11-3021.00'),
+('Computing Services Director','11-3021.00'),
+('Consulting Technical Director','11-3021.00'),
+('Cyber Policy and Strategy Planner','11-3021.00')
+,('Cyber Workforce Developer and Manager','11-3021.00'),
+('Data Center Manager','11-3021.00'),
+('Data Processing Manager','11-3021.00'),
+('Data Systems Manager','11-3021.00'),
+('Database Architect','11-3021.00'),
+('Director of Engineering','11-3021.00'),
+('Enterprise Resource Planner','11-3021.00'),
+('Health Informatics Specialist','11-3021.00'),
+('Health Information Administrator','11-3021.00'),
+('Health Information Technologist','11-3021.00'),
+('Information Services Manager','11-3021.00'),
+('Information Support Project Manager','11-3021.00'),
+('Information System and Data Processing Lead','11-3021.00'),
+('Information Systems Administrator','11-3021.00'),
+('Information Systems Director (IS Director)','11-3021.00'),
+('Information Systems Manager (IS Manager)','11-3021.00'),
+('Information Systems Planner (IS Planner)','11-3021.00'),
+('Information Systems Supervisor (IS Supervisor)','11-3021.00'),
+('Information Technology Account Manager (IT Account Manager)','11-3021.00'),
+('Information Technology Administrator (IT Administrator)','11-3021.00'),
+('Information Technology Analyst (IT Analyst)','11-3021.00'),
+('Information Technology Auditor (IT Auditor)','11-3021.00'),
+('Information Technology Consultant (IT Consultant)','11-3021.00'),
+('Information Technology Director (IT Director)','11-3021.00'),
+('Information Technology Manager (IT Manager)','11-3021.00'),
+('Information Technology Planning and Policy Analyst (IT Planning and Policy Analyst)','11-3021.00'),
+('Information Technology Program Management Specialist (IT Program Management Specialist)','11-3021.00'),
+('Interactive Media Project Manager','11-3021.00'),
+('Internet and E-Business Project Manager','11-3021.00'),
+('Internet Technology Manager','11-3021.00'),
+('IT Investment/Portfolio Manager','11-3021.00'),
+('IT Program Auditor','11-3021.00'),
+('Knowledge Manager','11-3021.00'),
+('MIS Director (Management Information Systems Director)','11-3021.00'),
+('MIS Specialist (Management Information Systems Specialist)','11-3021.00'),
+('Network Director','11-3021.00'),
+('Network Manager','11-3021.00'),
+('Product Support Manager','11-3021.00'),
+('Program Manager','11-3021.00'),
+('Programming and Software Development Project Manager','11-3021.00'),
+('Quality Assurance Manager (QA Manager)','11-3021.00'),
+('Reporting Analyst','11-3021.00'),
+('Software Development Director','11-3021.00'),
+('Software Project Manager','11-3021.00'),
+('System Development Manager','11-3021.00'),
+('Systems Applications Programming Lead','11-3021.00'),
+('Systems Engineering Manager','11-3021.00'),
+('Technical Director','11-3021.00'),
+('Technical Services Manager','11-3021.00'),
+('Technical Solutions Director','11-3021.00'),
+('Technology Coordinator','11-3021.00'),
+('Technology Director','11-3021.00')
+
+--=================================================================================
+insert into alternateTitles values('Administrative Analyst','13-1111.00')
+insert into alternateTitles values('Adviser Sales','13-1111.00')
+insert into alternateTitles values('Analyst Sales','13-1111.00')
+insert into alternateTitles values('Analyzer Sales','13-1111.00')
+insert into alternateTitles values('Business Analyst','13-1111.00')
+insert into alternateTitles values('Business Consultant','13-1111.00')
+insert into alternateTitles values('Business Development Analyst','13-1111.00')
+insert into alternateTitles values('Business Management Consultant','13-1111.00')
+insert into alternateTitles values('Business Management Consultant','13-1111.00')
+insert into alternateTitles values('Business Operations Analyst','13-1111.00')
+insert into alternateTitles values('Business Process Analyst','13-1111.00')
+insert into alternateTitles values('Business Process Consultant','13-1111.00')
+insert into alternateTitles values('Clerical Methods Analyst','13-1111.00')
+insert into alternateTitles values('Commercial Specialist','13-1111.00')
+insert into alternateTitles values('Dealer Analyst','13-1111.00')
+insert into alternateTitles values('Employment Programs Analyst','13-1111.00')
+insert into alternateTitles values('Forms Analysis Manager','13-1111.00')
+insert into alternateTitles values('Forms Analyst','13-1111.00')
+insert into alternateTitles values('Health Information Management Business Analyst','13-1111.00')
+
+
+
+insert into alternateTitles values('Health Information Management Data Analyst','13-1111.00')
+insert into alternateTitles values('Health Program Analyst','13-1111.00')
+insert into alternateTitles values('Health Program Specialist','13-1111.00')
+insert into alternateTitles values('Health Systems Analyst','13-1111.00')
+insert into alternateTitles values('Healthcare Consultant','13-1111.00')
+insert into alternateTitles values('Human Resource Analyst (HR Analyst)','13-1111.00')
+insert into alternateTitles values('Industrial Analyst','13-1111.00')
+insert into alternateTitles values('IT Analyst (Information Technology Analyst)','13-1111.00')
+insert into alternateTitles values('Leadership Development Manager','13-1111.00')
+insert into alternateTitles values('Management Analyst','13-1111.00')
+insert into alternateTitles values('Management and Program Analyst','13-1111.00')
+insert into alternateTitles values('Management Consultant','13-1111.00')
+insert into alternateTitles values('Management Scientist','13-1111.00')
+insert into alternateTitles values('Management Systems Auditor','13-1111.00')
+insert into alternateTitles values('Operations Analyst','13-1111.00')
+insert into alternateTitles values('Organizational Development Consultant','13-1111.00')
+insert into alternateTitles values('Performance Consultant','13-1111.00')
+insert into alternateTitles values('Price Analyst','13-1111.00')
+
+
+insert into alternateTitles values('Principal Consultant','13-1111.00')
+insert into alternateTitles values('Program Analyst','13-1111.00')
+insert into alternateTitles values('Program Development Specialist','13-1111.00')
+insert into alternateTitles values('Program Evaluator','13-1111.00')
+insert into alternateTitles values('Price Analyst','13-1111.00')
+insert into alternateTitles values('Program Management Analyst','13-1111.00')
+insert into alternateTitles values('Project Management Analyst','13-1111.00')
+insert into alternateTitles values('Purchase Analyst','13-1111.00')
+
+
+insert into alternateTitles values('Purchase Price Analyst','13-1111.00')
+insert into alternateTitles values('Quality Control Analyst','13-1111.00')
+insert into alternateTitles values('Records Analysis Manager','13-1111.00')
+insert into alternateTitles values('Records Management Analyst','13-1111.00')
+insert into alternateTitles values('Records Management Director','13-1111.00')
+insert into alternateTitles values('Reports Analysis Manager','13-1111.00')
+insert into alternateTitles values('Reports Analyst','13-1111.00')
+insert into alternateTitles values('Survey Analyst','13-1111.00')
+insert into alternateTitles values('Technical Analyst','13-1111.00')
+
+
+
+--===========================13-1199.02	Security Management Analysts=================================
+insert into alternateTitles values('Authorizing Official/Designating Representative','13-1199.02')
+insert into alternateTitles values('COMSEC Manager (Communications Security Manager)','13-1199.02')
+insert into alternateTitles values('Consultant','13-1199.02')
+insert into alternateTitles values('Cyber Defense Forensics Analyst','13-1199.02')
+insert into alternateTitles values('Director, Security Risk Management','13-1199.02')
+insert into alternateTitles values('Electronic Security Specialist','13-1199.02')
+insert into alternateTitles values('Forensics Analyst','13-1199.02')
+insert into alternateTitles values('Information Systems Security Manager','13-1199.02')
+insert into alternateTitles values('Operations Specialist','13-1199.02')
+insert into alternateTitles values('Operations Staff Specialist, Security','13-1199.02')
+insert into alternateTitles values('Personal Protection Specialist','13-1199.02')
+insert into alternateTitles values('Personal Security Specialist','13-1199.02')
+insert into alternateTitles values('Physical Security Engineer','13-1199.02')
+insert into alternateTitles values('Physical Security Manager','13-1199.02')
+insert into alternateTitles values('Physical Security Specialist','13-1199.02')
+insert into alternateTitles values('Section Chief, Physical Security Specialist','13-1199.02')
+insert into alternateTitles values('Security Administrator','13-1199.02')
+insert into alternateTitles values('Security Analyst','13-1199.02')
+insert into alternateTitles values('Security Compliance Analyst','13-1199.02')
+insert into alternateTitles values('Security Consultant','13-1199.02')
+insert into alternateTitles values('Security Control Assessor','13-1199.02')
+insert into alternateTitles values('Security Engineer','13-1199.02')
+insert into alternateTitles values('Security Management Specialist','13-1199.02')
+insert into alternateTitles values('Security Operations Analyst','13-1199.02')
+insert into alternateTitles values('Security Specialist','13-1199.02')
+insert into alternateTitles values('Security System Engineer','13-1199.02')
+insert into alternateTitles values('Security Systems Specialist','13-1199.02')
+insert into alternateTitles values('SOC Analyst','13-1199.02')
+insert into alternateTitles values('Workplace Violence Prevention Specialist','13-1199.02')
+
+
+
+--==========================15-1121.00	Computer Systems Analysts============================
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Systems Analyst','15-1121.00')
+insert into alternateTitles values('Automatic Data Processing Planner','15-1121.00')
+insert into alternateTitles values('Business Analyst','15-1121.00')
+insert into alternateTitles values('Business Systems Analyst','15-1121.00')
+insert into alternateTitles values('Chief Technology Officer (CTO)','15-1121.00')
+insert into alternateTitles values('Clinical Analyst','15-1121.00')
+insert into alternateTitles values('Computer Analyst','15-1121.00')
+insert into alternateTitles values('Computer Analyst Supervisor','15-1121.00')
+insert into alternateTitles values('Computer Architect','15-1121.00')
+insert into alternateTitles values('Computer Consultant','15-1121.00')
+insert into alternateTitles values('Computer Methods Analyst','15-1121.00')
+insert into alternateTitles values('Computer Systems Analyst','15-1121.00')
+insert into alternateTitles values('Computer Systems Consultant','15-1121.00')
+insert into alternateTitles values('Computer Systems Design Analyst','15-1121.00')
+insert into alternateTitles values('Computer Systems Designer','15-1121.00')
+insert into alternateTitles values('Cross-Enterprise Integrator','15-1121.00')
+insert into alternateTitles values('Cyber Defense Infrastructure Support Specialist','15-1121.00')
+insert into alternateTitles values('Data Processing Consultant','15-1121.00')
+insert into alternateTitles values('Data Processing Systems Consultant','15-1121.00')
+insert into alternateTitles values('Data Processing Systems Project Planner','15-1121.00')
+insert into alternateTitles values('Database Analyst','15-1121.00')
+insert into alternateTitles values('Database Design Analyst','15-1121.00')
+insert into alternateTitles values('Digital Computer Systems Analyst','15-1121.00')
+insert into alternateTitles values('E-Business Specialist','15-1121.00')
+insert into alternateTitles values('Engineering Systems Analyst','15-1121.00')
+insert into alternateTitles values('Enterprise Architect','15-1121.00')
+insert into alternateTitles values('ERP Technical Lead (Enterprise Resource Planning Technical Lead)','15-1121.00')
+insert into alternateTitles values('Functional Analyst','15-1121.00')
+insert into alternateTitles values('Health IT Specialist (Health Information Technology Specialist)','15-1121.00')
+
+
+insert into alternateTitles values('Health Systems Analyst
+','15-1121.00')
+insert into alternateTitles values('Implementation Consultant
+','15-1121.00')
+insert into alternateTitles values('Information Scientist
+','15-1121.00')
+insert into alternateTitles values('Information Systems Analyst (ISA)
+','15-1121.00')
+insert into alternateTitles values('Information Systems Architect
+','15-1121.00')
+insert into alternateTitles values('Information Systems Auditor
+','15-1121.00')
+insert into alternateTitles values('Information Systems Consultant
+','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
+insert into alternateTitles values('Applications Analyst','15-1121.00')
